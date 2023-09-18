436155724345c4a507585761acd09c756d193d38
Kevin <kramzyvine@icloud.com>
2023-09-18 13:31:30 +0300

    added regex.js

---
+++ regex.js
@@ -0,0 +1,2 @@
+console.table("ubvkuysbetvjkecgnesrufxhtvbxmoiosgevhiweuhxgbvjesv".match(/(i|x)/g));
+// so the count() method in python can be replicated in JavaScript using regex
--- sets.js
+++ sets.js
@@ -8,3 +8,25 @@ let total = 0;
 for (x of set_items.values()){total += Number(x)}
 let average = total/set_items.size;
 console.log(average.toFixed(3));
+
+
+/*Rupal has a huge collection of country stamps. She decided to count the total number of distinct 
+country stamps in her collection. She asked for your help. You pick the stamps one by one from a stack of
+country stamps.
+Find the total number of distinct country stamps.
+
+input:
+UK
+China
+USA
+France
+New Zealand
+UK
+France 
+
+output:
+5
+ */
+
+let countries = `UK China USA France Kenya UK France`;
+console.log(new Set(countries.split(" ")).size);
